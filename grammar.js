379d7fea4c407e71b784c8f0e4df2b871e368d4d
dansaldt <dansaldt@protonmail.com>
2023-12-13 15:59:37 +0700

    Implement function visibility, override, external and definition
    - keyword token separated into its own kw_XXXX
    - rename module level declaration_statement into module_item_declaration
    - function_forward_declaration, function_definition, and function_external under function_declaration
    -  some TODOs and incomplete expression

--- grammar.js
+++ grammar.js
@@ -1,18 +1,17 @@
 module.exports = grammar({
 	name: 'gold',
-
 	rules: {
 		source_file: $ => seq(
 			token('module'),
 			field('module_name', $.identifier),
-			repeat($.declaration_statement),
+			repeat($.module_item_declaration),
 		),
 
 		identifier: _ => /[_\p{XID_Start}][_\p{XID_Continue}]*/,
 
 		_type_identifier: $ => alias($.identifier, $.type_identifier),
 
-		declaration_statement: $ => choice(
+		module_item_declaration: $ => choice(
 			$.use_declaration,
 			$.type_declaration,
 			$.function_declaration,
@@ -29,30 +28,44 @@ module.exports = grammar({
 			// $.record_item,
 		),
 
-		function_declaration: $ => choice(
-			$.function_forward_declaration,
-			$.function_definition,
-		),
-
-		function_forward_declaration: $ => seq(
-			$.function_token_begin,
+		function_declaration: $ => seq(
+			$.kw_function_begin,
 			field('function_name', $.identifier),
 			optional($.function_parameters),
 			optional($.function_return_type),
-			token('forward'),
+			optional($.visibility),
+			optional($.kw_final),
+			optional($.kw_override),
+			choice(
+				$.function_forward_declaration,
+				$.function_definition,
+				$.function_external,
+			),
 		),
+
+		function_forward_declaration: $ => $.kw_forward,
 
 		function_definition: $ => seq(
-			$.function_token_begin,
-			field('function_name', $.identifier),
-			optional($.function_parameters),
-			optional($.function_return_type),
-			$.function_token_end,
+			optional($.function_body_statements),
+			$.kw_function_end,
 		),
 
-		function_token_begin: _ => token(choice('function', 'func', 'procedure', 'proc')),
+		function_external: $ => seq(
+			$.kw_external,
+			token('\''),
+			field('dll_name', $.identifier),
+			token('.'),
+			field('dll_function_name', $.identifier),
+			token('\''),
+		),
 
-		function_token_end: _ => token(choice('end', 'endFunc', 'endProc')),
+		kw_function_begin: _ => token(choice('function', 'func', 'procedure', 'proc')),
+
+		kw_function_end: $ => choice(
+			token('endFunc'),
+			token('endProc'),
+			$.kw_end,
+		),
 
 		function_parameters: $ => seq(
 			token('('),
@@ -69,9 +82,49 @@ module.exports = grammar({
 		),
 
 		function_return_type: $ => seq(
-			token('return'),
+			$.kw_return,
 			$._type_identifier,
 		),
+
+		function_body_statements: $ => seq(
+			repeat1($.statement)
+		),
+
+		variable_declaration: $ => seq(
+			token('var'),
+			field('variable_name', $.identifier),
+			token(':'),
+			field('variable_type', $._type_identifier),
+		),
+
+		statement: $ => choice(
+			$.declaration_statement,
+			// $.expression_statement,
+		),
+
+		declaration_statement: $ => choice(
+			$.use_declaration,
+			$.type_declaration,
+			$.variable_declaration,
+		),
+
+		// expression_statement: $ => {
+		//	// TODO: to be implemented
+		//},
+
+		visibility: $ => choice(
+			$.kw_private,
+			$.kw_protected,
+		),
+
+		kw_return: _ => token('return'),
+		kw_end: _ => token('end'),
+		kw_external: _ => token('external'),
+		kw_forward: _ => token('forward'),
+		kw_private: _ => token('private'),
+		kw_protected: _ => token('protected'),
+		kw_final: _ => token('final'),
+		kw_override: _ => token('override'),
 
 		enum_item: $ => seq(
 			'type',
